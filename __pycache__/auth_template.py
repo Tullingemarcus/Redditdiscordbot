a896d1b21b779cc09310cf5bfb41281906cf6a89
Tullingemarcus <marcus@tullingeskog.se>
2022-02-06 21:31:18 +0100

    adding auth_template file

---
+++ __pycache__/auth_template.py
@@ -0,0 +1,9 @@
+#Necessary that you will need to fill in
+#Your token for your discord bot can be found at https://discord.com/developers/applications
+#Under bot click copy token
+token = 'your_token'
+#The variables can be found at https://www.reddit.com/prefs/apps under your application
+PASSWORD = 'your_password'
+secret = 'your_client_secret'
+Id = 'your_client_id'
+name = 'your_reddit_username'
